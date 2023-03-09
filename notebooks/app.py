01f950401694315ab76a0b8da13d626fd203f2d5
Y.Ge <gylluckydove@gmail.com>
2023-03-09 23:12:26 +0100

    app.py added

---
+++ notebooks/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+from sentiment_generator_v2 import sentiment_generator
+
+def main():
+	image = Image.open('logo.png')
+	st.image(image)
+	st.title('Sentiment Generator')
+	st.subheader('Generate sentiment according to your comment about ChatGPT')
+	menu = ['Customer Experience Center']
+	choice = st.sidebar.selectbox('Menu',menu)
+	text = st.text_input('Please enter your comment')
+	output = sentiment_generator(text)
+	st.text(output)
+if __name__ == '__main__':
+	main()
